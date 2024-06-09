89b7eaa8fbc3d4e9e5a0fd7fece7d61e5ac66b73
Jhonier A <jhon.gutierrez@udla.edu.co>
2024-06-08 22:53:54 -0500

    Changes app

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://chat-front-henna.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
